096f0386d8fa21d922afa6a53a9b2c1cea7b1273
Jeffreypang911 <Jeffreypang911@gmail.com>
2018-12-28 10:42:12 -0800

    changes index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import "../node_modules/video-react/dist/video-react.css"; // import css
 import App from './App';
+import Carousel from './Components/Carousel';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Carousel />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
