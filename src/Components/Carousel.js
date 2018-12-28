ac6977b4e87432767ab61944393832aff24369b4
Jeffreypang911 <Jeffreypang911@gmail.com>
2018-12-28 14:25:50 -0800

    adds styling to CSS

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -9,24 +9,35 @@ class App extends Component {
           <div class="MovieRow__container">
             <div class="MovieBox">
               <div class="MovieBox__info">
-                <div class="MovieBox__info__title">Bachata Sensual: Movement Workshop</div>
-                <div>
-                  <i>"Wayne Kristal</i>
+                <div class="MovieBox__info__title">
+                  Bachata Sensual: Workshop
                 </div>
                 <div>
-                  Learn the fundamental movements necessary to get 
-                  <br />you to the next level 
-                  of bachata!
+                  <i>Wayne Kristal</i>
+                </div>
+                <div>
+                  Learn the fundamental movements necessary to get you to the next level of bachata!
                 </div>
               </div>
               <video autoplay muted loop id="myVideo" width="100%" height="100%" preload="auto" playsinline>
                 <source src="http://www.html5videoplayer.net/videos/toystory.mp4" type="video/mp4"></source>
-                Your browser does not support the video tag.
               </video>
-      
             </div>
             <div class="MovieBox">
-              MovieBox
+            <div class="MovieBox__info">
+                <div class="MovieBox__info__title">
+                  Beginner Bachata: Steps
+                </div>
+                <div>
+                  <i>"Wayne Kristal</i>
+                </div>
+                <div>
+                  Learn the fundamental movements necessary to get you to the next level of bachata!
+                </div>
+              </div>
+              <video autoplay muted loop id="myVideo" width="100%" height="100%" preload="auto" playsinline>
+                <source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4"></source>
+              </video>
             </div>
             <div class="MovieBox">
               MovieBox
